3c2d1ba8d31147c949db90a6589ed394650e8809
SAMIR-MALEK <samir.malek@univ-bba.dz>
2026-04-03 12:53:17 +0100

    Update prisma.js

--- backend/src/utils/prisma.js
+++ backend/src/utils/prisma.js
@@ -1,9 +1,11 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client')
 
-const prisma = global.prisma || new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
-});
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL + '?pgbouncer=true&connection_limit=1'
+    }
+  }
+})
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
-module.exports = prisma;
+module.exports = prisma
